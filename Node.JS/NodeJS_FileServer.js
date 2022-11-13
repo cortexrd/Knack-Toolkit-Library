bc92795809d1738e996adb2240e0af1ed1615e95
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2022-11-13 18:14:59 -0500

    Comments clean-up.

--- Node.JS/NodeJS_FileServer.js
+++ Node.JS/NodeJS_FileServer.js
@@ -1,10 +1,12 @@
+//Non-secure HTTP server, to be used on local network for development purpose only.
+//TODO:  Find out how to support space characters in file path.
 var fs = require('fs'),
     http = require('http');
 
 http.createServer(function (req, res) {
     fs.readFile(__dirname + req.url, function (err, data) {
-		console.log('__dirname =',__dirname);//$$$
-		console.log('req.url =',req.url);//$$$
+		console.log('__dirname =',__dirname);
+		console.log('req.url =',req.url);
 
         if (err) {
             res.writeHead(404);
