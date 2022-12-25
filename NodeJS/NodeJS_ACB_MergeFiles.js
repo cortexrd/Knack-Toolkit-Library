b5ebb6dad7cbd3fd41240c09fdb9f3c85f0aae60
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2022-12-25 10:07:02 -0500

    Merge script now includes CSS also.

--- NodeJS/NodeJS_ACB_MergeFiles.js
+++ NodeJS/NodeJS_ACB_MergeFiles.js
@@ -1,9 +1,10 @@
-//This is a small utility I've created to merge some Javascript files quickly.
+//This utility will merge the KTL and your custom Javascript and CSS files into two ACB files.
 //Modify the file names/paths to match your needs.
+//Version 1.0
 
 const fs = require('fs');
 
-var fileName = ''; //Required, must include the .js extension and have no spaces.
+var fileName = ''; //Required, without extension and no spaces.
 var appName = ''; //Optional, must and have no spaces.  If omitted, will use same as Knack app.
 var ktlPath = '..\\..\\Lib\\KTL'; //Optional, used when calling from a directory other than KnackApps.  If omitted, will use same default as per docs.
 
@@ -17,11 +18,9 @@ process.argv.forEach(function (val, index, array) {
     }
 });
 
-var mergedFile = fileName.replace('.js', '') + '_ACB.js'; //Output file.
-
-function mergeFiles(mergedFile, filesToMerge = []) {
-    if (!fileName) {
-        console.log('\n\nERROR: filename must be specified, with its .js extension.');
+function mergeFiles(fileName = '', ext = '', filesToMerge = []) {
+    if (!fileName || fileName.includes('.')) {
+        console.log('\n\nERROR: filename must be specified, without extension.');
         return;
     }
 
@@ -35,9 +34,21 @@ function mergeFiles(mergedFile, filesToMerge = []) {
         return;
     }
 
-    console.log('filename =', fileName);
-    console.log('appname =', appName ? appName : 'Same as Knack');
+    console.log('ext =', ext);
 
+    if (ext !== 'js' && ext !== 'css') {
+        console.log('\n\nERROR: extension must be js or css, without the dot.');
+        return;
+    }
+
+
+
+    const mergedFile = fileName + '_ACB.' + ext;
+    fileName += '.' + ext;
+
+    console.log('filename =', fileName);
+    console.log('ktlPath =', ktlPath);
+    console.log('appname =', appName ? appName : 'Same as Knack');
 
     if (filesToMerge.length) {
         try {
@@ -52,31 +63,39 @@ function mergeFiles(mergedFile, filesToMerge = []) {
             console.error(err)
         }
 
+        filesToMerge.push(fileName);
+        console.log('filesToMerge =', filesToMerge);
+
         var data = '';
         for (var index = 0; index < filesToMerge.length; index++) {
             var fileToMerge = filesToMerge[index];
             console.log('Merging file', fileToMerge);
             data = fs.readFileSync(fileToMerge, 'utf8');
 
-            if (index === 0) { //Bootloader
-                if (appName)
-                    data = data.replace("var appName = Knack.app.attributes.name;", "var appName = '" + appName + "';");
-                if (fileName)
-                    data = data.replace("var fileName = appName + '.js';", "var fileName = '" + fileName + "';");
+            if (ext === 'js') {
+                if (index === 0) { //Bootloader
+                    if (appName)
+                        data = data.replace("var appName = Knack.app.attributes.name;", "var appName = '" + appName + "';");
+                    if (fileName)
+                        data = data.replace("var fileName = appName + '.js';", "var fileName = '" + fileName + "';");
+                }
+
             }
 
             fs.appendFileSync(mergedFile, data);
+            console.log('\nMerged file =', mergedFile);
         }
     }
 
 }
 
 
-mergeFiles(mergedFile, [
+mergeFiles(fileName, 'js', [
     ktlPath + '\\KTL_Bootloader.js', //Order is important!
-    ktlPath + '\\KTL.js',
-    (fileName || (appName ? (appName + '.js') : ''))
-
+    ktlPath + '\\KTL.js'
 ]);
 
-console.log('\nOutput merged file =', mergedFile);
+mergeFiles(fileName, 'css', [
+    ktlPath + '\\KTL.css'
+]);
+
