29d308302b7fb3faabff6e76d9cef85ce2b037a7
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2023-01-19 19:59:17 -0500

    Merge only needs .js now, thanks to CDN.

--- Merge_ACB.bat
+++ Merge_ACB.bat
@@ -2,6 +2,7 @@
 REM *** Replace the c:\code folder below to match your system's settings.
 REM *** It is strongly recommended to use the folder structure as per documentation.
 REM *** Otherwise, you will need to modify this file accordingly.
-REM *** filename is required, without extension and no spaces.
+REM *** filename is required, without extension.  If it contains spaces, must be enclosed in quotes.
+
 cd c:\code
 node .\Lib\KTL\NodeJS\NodeJS_ACB_MergeFiles.js -ktlpath=.\Lib\KTL -filename=.\Lib\KTL\KTL_KnackApp
