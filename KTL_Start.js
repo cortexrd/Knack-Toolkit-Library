edb0f545d18437b461f11684b25b94e836e94199
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2023-01-28 10:18:33 -0500

    Added KTL_LATEST_VERSION and Prod folder.

--- KTL_Start.js
+++ KTL_Start.js
@@ -1,20 +1,18 @@
 //KTL and App Starter.  Also enables switching between Prod and Dev modes.
 //window.ktlStart = window.performance.now();
 window.lsShortName = Knack.app.attributes.name.substr(0, 6).replaceAll(' ', '') + '_' + app_id.substr(-4, 4) + '_';
-function loadKtl($, _callback, _KnackApp, _ktlVersion = '', _fullCode = '') {
+function loadKtl($, callback, _KnackApp, ktlVersion = '', fullCode = '') {
+    const KTL_LATEST_VERSION = '0.7.0';
+    var prodFolder = 'Prod/';
     var ktlSvr = 'https://ctrnd.com/'; //CDN is Cortex R&D Inc server.
-    var callback;
-    var ktlVersion = '';
-
     window.$ = $;
     window.jQuery = $; //For BlockUI
-    callback = _callback;
     window.KnackApp = _KnackApp;
-    ktlVersion = _ktlVersion;
-
+    ktlVersion = (ktlVersion ? ktlVersion : KTL_LATEST_VERSION);
     var prod = (localStorage.getItem(window.lsShortName + 'dev') === null);
     if (!prod) {
         ktlVersion = '';
+        prodFolder = '';
         var fileName = localStorage.getItem(window.lsShortName + 'fileName');
         !fileName && (fileName = Knack.app.attributes.name);
         ktlSvr = 'http://localhost:3000/';
@@ -28,8 +26,7 @@ function loadKtl($, _callback, _KnackApp, _ktlVersion = '', _fullCode = '') {
     LazyLoad.js(['https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.min.js']);
     LazyLoad.js(['https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js']);
     LazyLoad.css([ktlSvr + 'Lib/KTL/KTL.css'], () => { });
-    var ktlFile = ktlSvr + 'Lib/KTL/KTL' + (ktlVersion ? '-' + ktlVersion : '') + (_fullCode === 'full' ? '' : '.min') + '.js';
-    //console.log('ktlFile =', ktlFile);
+    var ktlFile = ktlSvr + 'Lib/KTL/' + prodFolder + (ktlVersion ? 'KTL-' + ktlVersion : 'KTL') + (fullCode === 'full' ? '' : '.min') + '.js';
     LazyLoad.js([ktlFile], () => {
         if (typeof Ktl === 'function') {
             if (prod) {
@@ -51,11 +48,12 @@ function loadKtl($, _callback, _KnackApp, _ktlVersion = '', _fullCode = '') {
             }
 
             callback();
-        } else
-            if (confirm('Error - can\'t load KTL.  Do you want to switch to Production?\n' + ktlFile)) {
+        } else {
+            if (confirm('Error - can\'t locate KTL file:\n' + ktlFile + '\n\nDo you want to switch to Production?')) {
                 localStorage.removeItem(window.lsShortName + 'dev');
                 location.reload(true);
             }
+        }
     })
 }
 
