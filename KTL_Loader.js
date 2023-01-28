d91e91a3a21cff8a255fbb31a5e9c9436f7cdac3
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2023-01-28 12:03:11 -0500

    Fixed intermittent with undefined callback.

--- KTL_Loader.js
+++ KTL_Loader.js
@@ -1,6 +1,6 @@
 KnackInitAsync = function ($, callback) { // Load the Knack Toolkit Library (KTL)
     (window.LazyLoad = LazyLoad) && LazyLoad.js(['https://ctrnd.com/Lib/KTL/KTL_Start.js'], () => {
-        loadKtl($, callback, KnackApp, '' /*KTL version, leave blank to get latest*/, ''/*Set to "full" for non-minified code*/);
+        loadKtl($, callback, (typeof KnackApp === 'function' ? KnackApp : null), '' /*KTL version, leave blank to get latest*/, 'full'/*Set to "full" for non-minified code*/);
     })
 };
 
