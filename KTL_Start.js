dda93b27a91df3859ed7a8b674c7ba50b31ea63c
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2023-01-28 12:02:39 -0500

    Fixed intermittent with undefined callback.
    Depends on account.  Some required this some don't.  Go figure...

--- KTL_Start.js
+++ KTL_Start.js
@@ -1,13 +1,15 @@
 //KTL and App Starter.  Also enables switching between Prod and Dev modes.
 //window.ktlStart = window.performance.now();
 window.lsShortName = Knack.app.attributes.name.substr(0, 6).replaceAll(' ', '') + '_' + app_id.substr(-4, 4) + '_';
-function loadKtl($, callback, _KnackApp, ktlVersion = '', fullCode = '') {
+var callback;
+function loadKtl($, _callback, _KnackApp, ktlVersion = '', fullCode = '') {
     const KTL_LATEST_VERSION = '0.7.0';
     var prodFolder = 'Prod/';
     var ktlSvr = 'https://ctrnd.com/'; //CDN is Cortex R&D Inc server.
     window.$ = $;
     window.jQuery = $; //For BlockUI
     window.KnackApp = _KnackApp;
+    callback = _callback;
     ktlVersion = (ktlVersion ? ktlVersion : KTL_LATEST_VERSION);
     var prod = (localStorage.getItem(window.lsShortName + 'dev') === null);
     if (!prod) {
@@ -30,24 +32,25 @@ function loadKtl($, callback, _KnackApp, ktlVersion = '', fullCode = '') {
     LazyLoad.js([ktlFile], () => {
         if (typeof Ktl === 'function') {
             if (prod) {
-                if (typeof KnackApp === 'function')
+                if (typeof KnackApp === 'function') {
                     KnackApp($, {});
-                else
+                    callback();
+                } else
                     LazyLoad.js([ktlSvr + 'Lib/KTL/KTL_KnackApp.js'], () => {
                         console.log('Loaded default KnackApp.');
                         KnackApp($, {});
+                        callback();
                     });
             } else { //Dev mode
-                if (typeof KnackApp === 'function')
+                if (typeof KnackApp === 'function') {
                     KnackApp($, { hostname: ktlSvr });
-                else {
+                    callback();
+                } else {
                     var fileName = prompt('Error - Cannot find KnackApp file.\nWhat is file name (without .js)?');
                     localStorage.setItem(window.lsShortName + 'fileName', fileName);
                     location.reload(true);
                 }
             }
-
-            callback();
         } else {
             if (confirm('Error - can\'t locate KTL file:\n' + ktlFile + '\n\nDo you want to switch to Production?')) {
                 localStorage.removeItem(window.lsShortName + 'dev');
