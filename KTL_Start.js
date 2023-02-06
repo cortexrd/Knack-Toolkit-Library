a24c6f67d254511952d8cbbc64c3d74b4ee38bc9
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2023-02-06 18:23:02 -0500

    Fixed replaceAll that's not always supported.
    And pass lsShortName as parameter to KnackApp.

--- KTL_Start.js
+++ KTL_Start.js
@@ -1,16 +1,18 @@
 //KTL and App Starter.  Also enables switching between Prod and Dev modes.
 //window.ktlStart = window.performance.now();
-window.lsShortName = Knack.app.attributes.name.substr(0, 6).replaceAll(' ', '') + '_' + app_id.substr(-4, 4) + '_';
-var callback;
 
 /* ktlVersion:
  *  - 'x,y,z' will use that specific Prod version.
  *  - if empty, will use the latest Prod version from KTL_LATEST_JS_VERSION.
  *  - if 'dev', will use /Prod/KTL-dev.js version, which is same as dev, copied by FTP.  See C:\code\FTP\WinSCP_Script.txt
 */
+
+var callback;
 function loadKtl($, _callback, _KnackApp, ktlVersion = '', fullCode = '') {
-    const KTL_LATEST_JS_VERSION = '0.7.7';
+    const KTL_LATEST_JS_VERSION = '0.7.9';
     const KTL_LATEST_CSS_VERSION = '0.2.1';
+
+
     var cssVersion = KTL_LATEST_CSS_VERSION;
     var prodFolder = 'Prod/';
     var ktlSvr = 'https://ctrnd.com/'; //CDN is Cortex R&D Inc server.
@@ -19,13 +21,14 @@ function loadKtl($, _callback, _KnackApp, ktlVersion = '', fullCode = '') {
     window.KnackApp = _KnackApp;
     callback = _callback;
     ktlVersion = (ktlVersion ? ktlVersion : KTL_LATEST_JS_VERSION);
-    var prod = (localStorage.getItem(window.lsShortName + 'dev') === null);
+    const lsShortName = Knack.app.attributes.name.substr(0, 6).replace(/ /g, '_') + '_' + app_id.substr(-4, 4) + '_';
+    var prod = (localStorage.getItem(lsShortName + 'dev') === null);
     if (!prod) {
         ktlVersion = '';
         cssVersion = '';
         prodFolder = '';
         fullCode = 'full';
-        var fileName = localStorage.getItem(window.lsShortName + 'fileName');
+        var fileName = localStorage.getItem(lsShortName + 'fileName');
         !fileName && (fileName = Knack.app.attributes.name);
         ktlSvr = 'http://localhost:3000/';
         var appUrl = ktlSvr + 'KnackApps/' + fileName + '/' + fileName + '.js';
@@ -48,28 +51,28 @@ function loadKtl($, _callback, _KnackApp, ktlVersion = '', fullCode = '') {
         if (typeof Ktl === 'function') {
             if (prod) {
                 if (typeof KnackApp === 'function') {
-                    KnackApp($, { ktlVersion: ktlVersion });
+                    KnackApp($, { ktlVersion: ktlVersion, lsShortName: lsShortName });
                     callback();
                 } else
                     LazyLoad.js([ktlSvr + 'Lib/KTL/KTL_KnackApp.js'], () => {
                         console.log('Loaded default KnackApp.');
-                        KnackApp($, { ktlVersion: ktlVersion });
+                        KnackApp($, { ktlVersion: ktlVersion, lsShortName: lsShortName });
                         callback();
                     });
             } else { //Dev mode
                 if (typeof KnackApp === 'function') {
-                    KnackApp($, { hostname: ktlSvr, ktlVersion: ktlVersion });
+                    KnackApp($, { hostname: ktlSvr, ktlVersion: ktlVersion, lsShortName: lsShortName });
                     callback();
                 } else {
                     var fileName = prompt('Error - Cannot find KnackApp file.\nWhat is file name (without .js)?');
-                    localStorage.setItem(window.lsShortName + 'fileName', fileName);
+                    localStorage.setItem(lsShortName + 'fileName', fileName);
                     location.reload(true);
                 }
             }
         } else {
             if (prod) {
                 console.log("Error - can't locate KTL file:\n" + ktlFile);
-                localStorage.removeItem(window.lsShortName + 'dev'); //JIC
+                localStorage.removeItem(lsShortName + 'dev'); //JIC
                 location.reload(true);
             }
         }
