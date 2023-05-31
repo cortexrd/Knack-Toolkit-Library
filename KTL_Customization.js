d33b4e94e3a748f15681b7f3c50a79f9da906ff6
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2023-05-31 17:11:20 -0400

    Removed useless IIFE.

--- KTL_Customization.js
+++ KTL_Customization.js
@@ -12,57 +12,55 @@ window.ktlReady = function (appInfo = {}) {
     ktl = window.ktl;
 
     //KTL Setup - BEGIN
-    (function () {
-        ktl.scenes.setCfg({
-            versionDisplayName: 'MyApp', //This will show a short version name of your app.  Leave empty to remove it.
-        })
+    ktl.scenes.setCfg({
+        versionDisplayName: 'MyApp', //This will show a short version name of your app.  Leave empty to remove it.
+    })
 
-        ktl.core.setCfg({
-            developerNames: ['Firstname Lastname'], //Add your name here to get super powers!
+    ktl.core.setCfg({
+        developerNames: ['Firstname Lastname'], //Add your name here to get super powers!
 
-            //Main KTL feature switches.  Here is where your App can override the defaults and enable/disable the features.
-            enabled: {
-                //"Basic" features
-                //Enabled by default. To disable them, their flag must exist and be set to true.
-                showAppInfo: true,
-                showKtlInfo: true,
-                showMenuInTitle: true,
-                selTextOnFocus: true,
-                inlineEditColor: true,
-                rowHoverHighlight: true,
-                chznBetter: true,
-                autoFocus: true,
-                sortedMenus: true,
-                userFilters: true,
-                persistentForm: true,
-                calendarGotoDate: true,
-                rememberMe: true,
-                formPreValidation: true,
-                spinnerWatchDog: true,
-                idleWatchDog: true,
-                debugWnd: true,
+        //Main KTL feature switches.  Here is where your App can override the defaults and enable/disable the features.
+        enabled: {
+            //"Basic" features
+            //Enabled by default. To disable them, their flag must exist and be set to true.
+            showAppInfo: true,
+            showKtlInfo: true,
+            showMenuInTitle: true,
+            selTextOnFocus: true,
+            inlineEditColor: true,
+            rowHoverHighlight: true,
+            chznBetter: true,
+            autoFocus: true,
+            sortedMenus: true,
+            userFilters: true,
+            persistentForm: true,
+            calendarGotoDate: true,
+            rememberMe: true,
+            formPreValidation: true,
+            spinnerWatchDog: true,
+            idleWatchDog: true,
+            debugWnd: true,
 
-                //"Advanced" features
-                //Disabled by default and/or must be set up properly to have any effect. See documentation.
-                iFrameWnd: false,
-                logging: {
-                    logins: false,
-                    navigation: false,
-                    activity: false,
-                },
-
-                bulkOps: {
-                    bulkEdit: true,
-                    bulkCopy: true,
-                    bulkDelete: true,
-                },
+            //"Advanced" features
+            //Disabled by default and/or must be set up properly to have any effect. See documentation.
+            iFrameWnd: false,
+            logging: {
+                logins: false,
+                navigation: false,
+                activity: false,
             },
-        })
-    }());
+
+            bulkOps: {
+                bulkEdit: true,
+                bulkCopy: true,
+                bulkDelete: true,
+            },
+        },
+    })
+
     //KTL Setup - END
 
     /////////////////////////////////////////////////
     //Your App-specific code goes here...
 
 }
-
