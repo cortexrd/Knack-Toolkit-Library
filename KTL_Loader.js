1b90d35ad57248ade16a9969654baa3dc2f76744
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2023-06-20 10:27:16 -0400

    Changed server from ctrnd.com to AWS S3 Bucket.
    Fixes instability problems due to overloading and exceeding request limits.
    
    KTL is victim of its own success I guess!!!

--- KTL_Loader.js
+++ KTL_Loader.js
@@ -1,5 +1,5 @@
 KnackInitAsync = function ($, callback) { // Load the Knack Toolkit Library (KTL)
-    (window.LazyLoad = LazyLoad) && LazyLoad.js(['https://ctrnd.com/Lib/KTL/KTL_Start.js'], () => {
+    (window.LazyLoad = LazyLoad) && LazyLoad.js(['https://ctrnd.s3.amazonaws.com/Lib/KTL/KTL_Start.js'], () => {
         loadKtl($, callback, (typeof KnackApp === 'function' ? KnackApp : null), '' /*KTL version, leave blank to get latest*/, 'min'/*min or full*/);
     })
 };
