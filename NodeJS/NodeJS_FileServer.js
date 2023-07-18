417beb3c048da1646f527dce8dc4a58620e7b093
Charles Brunelle <charles@brunelle.io>
2023-07-18 07:52:14 -0400

    Adjusted node file server for documented tree

--- NodeJS/NodeJS_FileServer.js
+++ NodeJS/NodeJS_FileServer.js
@@ -1,10 +1,11 @@
 //Non-secure HTTP server, to be used on local network for development purpose only.
-var fs = require('fs'),
-    http = require('http');
+const fs = require('fs');
+const http = require('http');
+const path = require('path')
 
 console.log('Starting server, waiting for requests...');
 
-const root = process.argv[2] || process.cwd();
+const root = process.argv[2] || path.join(process.cwd(), '..', '..');
 
 console.log(`Serving files from ${root}`);
 
