2e7aa2fd3fa0f74388d374b225ffc9f594dfc880
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2023-08-18 14:03:03 -0400

    Hotfix for KTL Developer Tools header drag n drop.

--- KTL_Start.js
+++ KTL_Start.js
@@ -9,7 +9,7 @@
 
 var callback;
 function loadKtl($, _callback, _KnackApp, ktlVersion = '', fullCode = '') {
-    const KTL_LATEST_JS_VERSION = '0.15.3';
+    const KTL_LATEST_JS_VERSION = '0.15.4';
     const KTL_LATEST_CSS_VERSION = '0.3.0';
 
     var cssVersion = KTL_LATEST_CSS_VERSION;
