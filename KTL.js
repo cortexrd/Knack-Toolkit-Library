f8f9d2349e2d5c5aff19647b9addb9e9da42db52
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2023-10-21 17:33:52 -0400

    Lots of big changes. See description.
    Parser now support keywords in Rich Text views.
    Optimizations of several keywords' processing pre-render to reduce flickering.
    Added class ktlVisibilityHidden to hide views temporarily, until all hidden/show conditions are done evaluating.
    Lots of improvements on _oln and _ols.
    New addLongClickListener to hold mouse 500ms before doing an action.
    Long press on any view title causes a refresh of that view.

--- KTL.js
+++ KTL.js
@@ -21,7 +21,7 @@ function Ktl($, appInfo) {
     if (window.ktl)
         return window.ktl;
 
-    const KTL_VERSION = '0.16.4';
+    const KTL_VERSION = '0.17.0';
     const APP_KTL_VERSIONS = window.APP_VERSION + ' - ' + KTL_VERSION;
     window.APP_KTL_VERSIONS = APP_KTL_VERSIONS;
 
