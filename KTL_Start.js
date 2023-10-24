f4d3bb3d5060314fdfaff8d31cdadc729e442e74
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2023-10-24 14:30:53 -0400

    User Filters issue #30 and _oln fixes.

--- KTL_Start.js
+++ KTL_Start.js
@@ -9,7 +9,7 @@
 
 var callback;
 function loadKtl($, _callback, _KnackApp, ktlVersion = '', fullCode = '') {
-    const KTL_LATEST_JS_VERSION = '0.17.0';
+    const KTL_LATEST_JS_VERSION = '0.17.1';
     const KTL_LATEST_CSS_VERSION = '0.4.4';
 
     var cssVersion = KTL_LATEST_CSS_VERSION;
