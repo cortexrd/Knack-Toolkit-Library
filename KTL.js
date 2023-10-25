5411570333e1a55ba1cd7aeb47e9202c8fcd69fb
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2023-10-25 15:34:18 -0400

    Fixed issue #132 - date formats.
    Added addEditKioskPageBtn in Dev Tools.

--- KTL.js
+++ KTL.js
@@ -21,7 +21,7 @@ function Ktl($, appInfo) {
     if (window.ktl)
         return window.ktl;
 
-    const KTL_VERSION = '0.17.1';
+    const KTL_VERSION = '0.17.2';
     const APP_KTL_VERSIONS = window.APP_VERSION + ' - ' + KTL_VERSION;
     window.APP_KTL_VERSIONS = APP_KTL_VERSIONS;
 
