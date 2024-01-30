be3ab8dbed0423c1b54ab69cf6db246f635a4a71
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2024-01-29 20:34:35 -0500

    Hide bulkEditCb in print mode.

--- KTL_Start.js
+++ KTL_Start.js
@@ -10,7 +10,7 @@
 var callback;
 function loadKtl($, _callback, _KnackApp, ktlVersion = '', fullCode = '') {
     const KTL_LATEST_JS_VERSION = '0.22.14';
-    const KTL_LATEST_CSS_VERSION = '0.6.5';
+    const KTL_LATEST_CSS_VERSION = '0.6.6';
 
     var cssVersion = KTL_LATEST_CSS_VERSION;
     var prodFolder = 'Prod/';
