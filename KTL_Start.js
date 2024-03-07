7fb9ead3524a2f10b9b7fa376a5b755f45dbbb07
Cortex R&D Inc <42851359+cortexrd@users.noreply.github.com>
2024-03-07 11:40:20 -0500

    Put back CSS like it was regarding ktlHidden_
    It's dis_ktlHidden_ with "dis" at the beginning, not at the end.

--- KTL_Start.js
+++ KTL_Start.js
@@ -11,7 +11,7 @@
 var callback;
 function loadKtl($, _callback, _KnackApp, ktlVersion = '', fullCode = '') {
     const KTL_LATEST_JS_VERSION = '0.24.1';
-    const KTL_LATEST_CSS_VERSION = '0.7.3';
+    const KTL_LATEST_CSS_VERSION = '0.7.4';
 
     var cssVersion = KTL_LATEST_CSS_VERSION;
     var prodFolder = 'Prod/';
