783b4296e92230a748177bae9a73d3d3543674a3
CSWinnall <craig.winnall@gmail.com>
2024-04-28 19:24:27 +0100

    Fix tooltip positioning and styling (#284)
    * Fix tooltip positioning and styling
    
    * Add .data to retrieve the tooltip element in the mouseleave
    
    * Remove a comment

--- KTL.js
+++ KTL.js
@@ -12025,38 +12025,43 @@ function Ktl($, appInfo) {
                     }
                 }
 
-                $(document).on('mouseenter', `${tooltipIconPosition} i.${tooltipIcon}`, function (e) {
-                    if (!$('.ktlTooltip').length) {
-                        const tooltipElement = $(`<div class="ktlTooltip ktlTtip-${viewType}-view">${tooltipText}</div>`);
-                        tooltipElement.appendTo('body');
+                $(`${tooltipIconPosition} i.${tooltipIcon}`).on('mouseenter.ktlTooltip', function (e) {
+                    const icon = $(this);
 
-                        const tooltipWidth = tooltipElement.outerWidth();
-                        const tooltipHeight = tooltipElement.outerHeight();
+                    const tooltipElement = $(`<div class="ktlTooltip ktlTtip-${viewType}-view">${tooltipText}</div>`).appendTo('body');
+                    const tooltipWidth = tooltipElement.outerWidth();
+                    const tooltipHeight = tooltipElement.outerHeight();
 
-                        let tooltipLeft = e.clientX - tooltipWidth / 2;
-                        let tooltipTop = e.clientY - tooltipHeight - 20;
+                    let tooltipLeft = e.clientX - tooltipWidth / 2;
+                    let tooltipTop = e.clientY - tooltipHeight - 20;
 
-                        if (tooltipLeft < 0) {
-                            tooltipLeft = 10;
-                        } else if (tooltipLeft + tooltipWidth > $(window).width()) {
-                            tooltipLeft = $(window).width() - tooltipWidth - 10;
-                        }
-
-                        if (tooltipTop < 0) {
-                            tooltipTop = e.clientY + 20;
-                        }
-
-                        tooltipElement.css({
-                            position: 'fixed',
-                            left: tooltipLeft + 'px',
-                            top: tooltipTop + 'px',
-                            zIndex: 1000
-                        });
+                    if (tooltipLeft < 0) {
+                        tooltipLeft = 10;
+                    } else if (tooltipLeft + tooltipWidth > $(window).width()) {
+                        tooltipLeft = $(window).width() - tooltipWidth - 10;
                     }
-                });
 
-                $(document).on('mouseleave', `${tooltipIconPosition} i.${tooltipIcon}`, function () {
-                    $('.ktlTooltip').remove();
+                    if (tooltipTop < 0) {
+                        tooltipTop = e.clientY + 20;
+                    }
+
+                    // Set the style properties directly on the element's style object
+                    Object.assign(tooltipElement[0].style, {
+                        position: 'fixed',
+                        left: tooltipLeft + 'px',
+                        top: tooltipTop + 'px',
+                        zIndex: 2000
+                    });
+
+                    icon.data('tooltipElement', tooltipElement);
+
+                    tooltipElement.show();
+                }).on('mouseleave.ktlTooltip', function () {
+                    const icon = $(this);
+                    // Retrieve the tooltipElement from the icon
+                    const tooltipElement = icon.data('tooltipElement')
+
+                    tooltipElement.remove();
                 });
             },
 
