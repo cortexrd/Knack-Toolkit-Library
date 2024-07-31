ee05aec2c46f3103a135cff178d6ba6494df4c8d
Cortex R&D Inc. <42851359+cortexrd@users.noreply.github.com>
2024-07-31 09:26:45 -0400

    V-bump

--- KTL_Start.js
+++ KTL_Start.js
@@ -10,7 +10,7 @@
 
 var callback;
 function loadKtl($, _callback, _KnackApp, ktlVersion = '', fullCode = '') {
-    const KTL_LATEST_JS_VERSION = '0.27.10';
+    const KTL_LATEST_JS_VERSION = '0.27.11';
     const KTL_LATEST_CSS_VERSION = '0.7.12';
 
     var cssVersion = KTL_LATEST_CSS_VERSION;
