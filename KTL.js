8baf276cc222dc01ff43389f81c7918afa6c27ff
Cortex R&D Inc. <42851359+cortexrd@users.noreply.github.com>
2024-08-19 01:34:55 -0400

    V-bump

--- KTL.js
+++ KTL.js
@@ -21,7 +21,7 @@ function Ktl($, appInfo) {
     if (window.ktl)
         return window.ktl;
 
-    const KTL_VERSION = '0.28.2';
+    const KTL_VERSION = '0.28.3';
     const APP_KTL_VERSIONS = window.APP_VERSION + ' - ' + KTL_VERSION;
     window.APP_KTL_VERSIONS = APP_KTL_VERSIONS;
 
