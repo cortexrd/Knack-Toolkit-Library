3ab3a7f640c87818c1baceb07d7b6626cdb2dba4
Craig Winnall <craig.winnall@gmail.com>
2024-11-18 15:01:21 +0000

    If a view is hidden on open renderSignatures() When a view is hidden signature boxes were not rendering as Knack check if visible

--- KTL.js
+++ KTL.js
@@ -12050,6 +12050,10 @@ ${objectData.map(obj => `${obj.name.padEnd(maxNameLength)} | ${obj.fieldCount.to
                         if (keywordsArray.length) {
                             ktl.views.stickTableHeader(viewId);
                         }
+                        // Check for .kn-input-signature before triggering renderSignatures
+                        if ($('.kn-input-signature').length && Knack.views[viewId] && typeof Knack.views[viewId].renderSignatures === 'function') {
+                            Knack.views[viewId].renderSignatures();
+                        }
                     });
                     arrowSelector.removeClass('ktlDown').addClass('ktlUp');
                     buttonSelector.addClass('ktlActive');
