cb0c00667862ad3a8d3dd5127f6efe7f4cafcbf0
Craig Winnall <craig.winnall@gmail.com>
2024-12-03 15:18:56 +0000

    Fixed grid groups having colspan = 0  and added a triger to the open of the view (#418)
    * Fixed grid groups having colspan = 0  and added a triger to the open of the view
    
    * Minor update

--- KTL.js
+++ KTL.js
@@ -12129,9 +12129,18 @@ function Ktl($, appInfo) {
                             ktl.views.stickTableHeader(viewId);
                         }
 
-                        if ($('.kn-input-signature').length && Knack.views[viewId] && typeof Knack.views[viewId].renderSignatures === 'function') {
+                        const signatureElements = viewElement.find('.kn-input-signature');
+                        const tableGroupCell = viewElement.find('.kn-table-group td');
+
+                        if (signatureElements.length && Knack.views[viewId] && typeof Knack.views[viewId].renderSignatures === 'function') {
                             Knack.views[viewId].renderSignatures();
                         }
+
+                        if (tableGroupCell.attr('colspan') === '0' && Knack.views[viewId] && typeof Knack.views[viewId].render === 'function') {
+                            Knack.views[viewId].render();
+                        }
+
+                        $(document).trigger('KTL.hideShowView.Opened', { viewId });
                     });
                     arrowSelector.removeClass('ktlDown').addClass('ktlUp');
                     buttonSelector.addClass('ktlActive');
