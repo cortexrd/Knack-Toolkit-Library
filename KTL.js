d9b16ddb586c6efffcf4f2bd59be5404193adf8f
Craig Winnall <craig.winnall@gmail.com>
2025-03-31 09:09:04 +0100

    Fixe for reports

--- KTL.js
+++ KTL.js
@@ -7371,7 +7371,15 @@ function Ktl($, appInfo) {
 
                 $(document).trigger(`KTL.summaryReady.${viewId}`);
             }
-        })
+
+            iterateViewReports(view, (report, keywords) => {
+                if (keywords) {
+                    keywords._sth && stickyReportTableHeader(report, keywords);
+                    keywords._stc && stickyReportTableColumns(report, keywords);
+                }
+            });
+
+        });
 
         function summaryPostProcessing(view, data) {
             const viewId = view.key;
@@ -8145,25 +8153,7 @@ function Ktl($, appInfo) {
 
                 for (let i = 0; i < paramGroups.length; i += 2) {
                     const [firstParam, [viewOptionTxt, ttipIcon]] = paramGroups.slice(i, i + 2);
-                    const ttipText = (() => {
-                        const baseText = firstParam
-                            .map(item => item.trim())
-                            .join(', ');
-
-                        const replacements = {
-                            '{br}': '<br>',
-                            '{strong}': '<strong>',
-                            '{/strong}': '</strong>',
-                            '{em}': '<em>',
-                            '{/em}': '</em>',
-                            '{hr}': '<hr>',
-                        };
-
-                        return Object.entries(replacements).reduce(
-                            (text, [pattern, replacement]) => text.replaceAll(pattern, replacement),
-                            baseText
-                        );
-                    })();
+                    const ttipText = firstParam.map(item => item.trim()).join(', ');
                     const tooltipIcon = ttipIcon || 'fa-question-circle';
 
                     ['f', 'l', 'd', 't'].forEach(option => {
@@ -15669,10 +15659,26 @@ function Ktl($, appInfo) {
                     }
                 }
 
+                const ttipText = (() => {
+                    const replacements = {
+                        '{br}': '<br>',
+                        '{strong}': '<strong>',
+                        '{/strong}': '</strong>',
+                        '{em}': '<em>',
+                        '{/em}': '</em>',
+                        '{hr}': '<hr>',
+                    };
+
+                    return Object.entries(replacements).reduce(
+                        (text, [pattern, replacement]) => text.replaceAll(pattern, replacement),
+                        tooltipText
+                    );
+                })();
+
                 $(`${tooltipIconPosition} i.${tooltipIcon}`).on('mouseenter.ktlTooltip', function (e) {
                     const icon = $(this);
 
-                    const tooltipElement = $(`<div class="ktlTooltip ktlTtip-${viewType}-view">${tooltipText}</div>`).appendTo('body');
+                    const tooltipElement = $(`<div class="ktlTooltip ktlTtip-${viewType}-view">${ttipText}</div>`).appendTo('body');
                     const tooltipWidth = tooltipElement.outerWidth();
                     const tooltipHeight = tooltipElement.outerHeight();
 
