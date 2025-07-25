9f2ceaa1d05ba1a8e967110129b0360b5eb4419d
Cortex R&D Inc. <42851359+cortexrd@users.noreply.github.com>
2025-07-25 12:04:11 -0400

    Fixed kiosk issue, unable to auto-login.

--- KTL.js
+++ KTL.js
@@ -21,7 +21,7 @@ function Ktl($, appInfo) {
     if (window.ktl)
         return window.ktl;
 
-    const KTL_VERSION = '0.32.6';
+    const KTL_VERSION = '0.32.7';
     const APP_KTL_VERSIONS = window.APP_VERSION + ' - ' + KTL_VERSION;
     window.APP_KTL_VERSIONS = APP_KTL_VERSIONS;
 
@@ -19251,7 +19251,7 @@ function Ktl($, appInfo) {
                                 const currentURL = window.location.href;
                                 for (const displayKey in svrResponse.deviceInfo.displays) {
                                     const display = svrResponse.deviceInfo.displays[displayKey];
-                                    if (display.URL && currentURL.includes(display.URL)) {
+                                    if (display.URL && currentURL.includes(display.URL.replace(/ --kiosk$/, ''))) {
                                         if (display.email && display.password) {
                                             credentials = {
                                                 email: display.email,
