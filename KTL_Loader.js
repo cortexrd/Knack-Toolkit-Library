2e873c06012497cedad489c8b65f0b695424744b
Cortex R&D Inc. <42851359+cortexrd@users.noreply.github.com>
2026-02-08 12:22:43 -0500

    Remove min/full parameter from loadKtl calls, bump Configurator to 1.3.0

--- KTL_Loader.js
+++ KTL_Loader.js
@@ -1,6 +1,5 @@
 KnackInitAsync = function ($, callback) { // Load the Knack Toolkit Library (KTL)
     (window.LazyLoad = LazyLoad) && LazyLoad.js([`https://ctrnd.s3.amazonaws.com/Lib/KTL/KTL_Start.js?${new Date().valueOf()}`], () => {
-        loadKtl($, callback, (typeof KnackApp === 'function' ? KnackApp : null), '' /*KTL version, leave blank to get latest*/, 'min'/*min or full*/);
+        loadKtl($, callback, (typeof KnackApp === 'function' ? KnackApp : null), '' /*KTL version, leave blank to get latest*/);
     })
 };
-
