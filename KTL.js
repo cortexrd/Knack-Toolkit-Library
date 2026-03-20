cd9ecd91c6cc5ad4170908bff5b9d748a222d60e
Cortex R&D Inc. <42851359+cortexrd@users.noreply.github.com>
2026-03-20 17:54:24 -0400

    Fix tooltip selector to use jQuery object instead of querySelector
    querySelector fails on jQuery-specific pseudo-selectors like :textEquals.
    Reuse the already-resolved jQuery pos object instead. V-bump to 0.42.5.

--- KTL.js
+++ KTL.js
@@ -22,7 +22,7 @@ function Ktl($, appInfo) {
     if (window.ktl)
         return window.ktl;
 
-    const KTL_VERSION = '0.42.4';
+    const KTL_VERSION = '0.42.5';
     const APP_KTL_VERSIONS = window.APP_VERSION + ' - ' + KTL_VERSION;
     window.APP_KTL_VERSIONS = APP_KTL_VERSIONS;
 
@@ -23273,7 +23273,7 @@ function Ktl($, appInfo) {
 
                 const ttipText = this.processTextMarkup(tooltipText);
 
-                const posEl = document.querySelector(tooltipIconPosition);
+                const posEl = pos.get(0);
                 if (posEl) posEl.dataset.ktlTtipText = ttipText;
 
                 $(`${tooltipIconPosition} i.${tooltipIcon}`).on('mouseenter.ktlTooltip', function (e) {
